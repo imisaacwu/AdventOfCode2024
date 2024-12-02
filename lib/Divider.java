cf2d6d25739d53aa481dce3807f35bf0a6546840
Isaac <isaacw1925@gmail.com>
2024-12-02 00:19:48 -0800

    Add Grid structure and Divider interface for generic input handling

---
+++ lib/Divider.java
@@ -0,0 +1,29 @@
+package lib;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// Interface to initialize Grid
+public interface Divider<T> {
+    List<T> toList(String s);
+    
+    public static class Int implements Divider<Integer> {
+        public List<Integer> toList(String s, String delim) {
+            return Arrays.stream(s.split(delim)).mapToInt(c -> Integer.parseInt(c)).boxed().collect(Collectors.toList());
+        }
+    
+        @Override
+        public List<Integer> toList(String s) {
+            return toList(s, " ");
+        }
+    }
+
+    public static class Char implements Divider<Character> {
+        @Override
+        public List<Character> toList(String s) {
+            return s.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
+        }
+    }
+}
+
