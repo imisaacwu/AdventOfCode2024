60d7cd187b61d5e6011d0371e33b0800cd123d2b
Isaac <isaacw1925@gmail.com>
2024-12-01 21:35:49 -0800

    Day 2 Solutions

---
+++ src/day02.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import lib.FileIO;
+
+public class day02 {
+    public static void main(String[] args) {
+        List<String> input = FileIO.read("input/day02.in");
+        int safe = 0, safeDampened = 0;
+
+        for (String s : input) {
+            List<Integer> report = Arrays.stream(s.split(" ")).mapToInt(c -> Integer.parseInt(c)).boxed().collect(Collectors.toList());
+            if (isSafe(report)) {
+                safe++;
+                safeDampened++;
+                continue;
+            } else {
+                for(int i = 0; i < report.size(); i++) {
+                    int removed = report.remove(i);
+                    if (isSafe(report)) {
+                        safeDampened++;
+                        break;
+                    }
+                    report.add(i, removed);
+                }
+            }
+        }
+
+        System.out.printf("Part 1: %d\n", safe);
+        System.out.printf("Part 2: %d\n", safeDampened);
+    }
+
+    static boolean isSafe(List<Integer> report) {
+        boolean increasing = report.get(0) < report.get(1);
+        for (int i = 1; i < report.size(); i++) {
+            int diff = Math.abs(report.get(i-1) - report.get(i));
+            if (diff < 0 || diff > 3 ||
+                (increasing && report.get(i-1) > report.get(i)) ||
+                (!increasing && report.get(i-1) < report.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
